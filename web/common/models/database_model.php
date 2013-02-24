9aa983fa74dd79a46493ce3587daf43894de4ab0
GAPLabs <GAPLabs@iMac-CA.local>
2013-02-24 09:44:11 +0800

    adde database and top apps model

---
+++ web/common/models/database_model.php
@@ -0,0 +1,27 @@
+<?php
+	Class Database{
+
+		private $con;
+
+		function __construct(){
+			$this->con = new PDO( "mysql:host=localhost;dbname=up;", "root", "" );
+		}
+		
+		/**
+		* Function that returns a prepared statement based on the prepared query <code>$query</code> 
+		*/
+		function create_prepared_statement( $query ){
+			if(isset($this->con) && $this->con != NULL)
+				return $this->con->prepare( $query );
+			else
+				return FALSE;
+		}
+
+		/**
+		* Function to clo
+		*/
+		function close(){
+			$this->con = NULL;
+		}
+	}
+?>
---
+++ web/common/models/top_apps_model.php
@@ -0,0 +1,11 @@
+<?php
+	class Top_apps{
+		function __construct(){
+			private $models = "common/models", ;
+			function get_top_five(){
+				require_once($this->models."/database.php");
+				
+			}
+		}
+	}
+?>
