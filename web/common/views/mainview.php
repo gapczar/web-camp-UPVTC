9dd6756ca9180c90643c83dd147cef5aff1f7f86
GAPLabs <GAPLabs@Elie.local>
2013-02-24 13:03:38 +0800

    Updated views

--- web/common/views/mainview.php
+++ web/common/views/mainview.php
@@ -1,43 +1,26 @@
-<?php
-	require_once('common/views/nav.php');
-?>
-<html>
-<head>
-	<title>Poll Website for Mobile Apps</title>
-	<link rel="stylesheet" type="text/css" href="<?php echo Util::base_url('css/style.css'); ?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo Util::base_url('assets/bootstrap/css/bootstrap.min.css'); ?>">
-
-</head>
-<body>
-
-
 	<div class="container">
 		<div class="row">
-			<div class="span12">
-				<h1>Vote for your fave web apps now!</h1>
-			</div>
+			
+				<h1>Vote for your fave mobile apps now!</h1>
 		</div>
 		
-		
-			<div class="row span10 signup">
-				<div class="span4">
-					<p>Create Your Account</p>
+		<div class="row signup">
+			<div class="span2"></div>
+			<div class="span6 hero-unit">
+				<div class="row">
+					<div class="span3 instruction">
+						<p>Create Your Account</p>
+					</div>
+					<div class="span2" id="forms">
+						<p><input style="text" placeholder="Username"/></p>
+						<p><input style="text" placeholder="Password"/></p>
+						<p><input style="text" placeholder="Email Address"/></p>
+						<p><input type="submit" value="Continue"/></p>
+					</div>
 				</div>
-				<div class="span4">
-					<form method="post" action="../controllers/user_controller.php">
-					<p><input type="text" name="username" style="text" placeholder="Username"/></p>
-					<p><input type="password" name="password" style="text" placeholder="Password"/></p>
-					<p><input type="text" name="email" style="text" placeholder="Email Address"/></p>
-					<p><input type="submit" value="Sign up!" /></p>
-					</form>
-				</div>
+				<div class="row">
+					Already have an account? Login <a href=""> here. </a>
+				</div>	
 			</div>
-		
-		<div class="row">
-			<div class="span12">
-				Already have an account? Login <a href=""> here. </a>
-			</div>
-		</div>
 	</div>
-</body>
-</html>
+	</div>
