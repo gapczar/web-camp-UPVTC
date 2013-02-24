c67ed4c64df0642627eeaef97967ce0b89232706
GAPLabs <GAPLabs@Raphs-Mac-mini.local>
2013-02-24 11:38:01 +0800

    design pages

---
+++ web/design/nav.php
@@ -0,0 +1,12 @@
+<div class="navbar navbar-inverse navbar-fixed-top">
+	<div class="navbar-inner">
+		<div class="container">
+			<a class="brand" href="">Mobile Apps Poll</a>
+			<p class="navbar-text pull-right">myUsername
+			<a href="">Logout</a>
+		</p>
+		</div>
+
+	</div>
+</div>
+
--- web/design/top5.php
+++ web/design/top5.php
@@ -12,28 +12,24 @@
 
 
 	<div class="container">
-		<div class="row">
+		<div class="hero-unit">
+<div class="row">
 			
-				<h1>Choose Your Top 5 Apps!</h1>
-				<h2>Remaining Votes: 5</h2>
+				<h1>Your Top 5 Apps</h1>
 			
 		</div>
-
-<div class="hero-unit">
-	<?php for($rowCount = 1; $rowCount <= 10; $rowCount++):?>
 		<div class="row">
 			<?php for($colCount = 1; $colCount <= 5; $colCount++):?>
 				<div class="image"><img src="design/img.png"/>
 					<p>Plants vs. Zombies</p>
-					<input type="submit" value="Vote" />
-					<input type="submit" value="Unvote" disabled/>
+
 				</div>
 			<?php endfor;?>
 		</div>
-	<?php endfor;?>
 
 
-</div>
+		</div>
+		
 		
 
 	</div>
