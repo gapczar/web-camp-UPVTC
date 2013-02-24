069b8fcef3d2889fd42a1bb1df1bddf45d6e1e60
GAPLabs <GAPLabs@Elie.local>
2013-02-24 13:44:51 +0800

    Updated header

--- web/common/views/header.php
+++ web/common/views/header.php
@@ -4,6 +4,8 @@
 
 	<link rel="stylesheet" type="text/css" href="<?php echo Util::base_url('assets/bootstrap/css/bootstrap.min.css');?>"/>
 	<link rel="stylesheet" type="text/css" href="<?php echo Util::base_url('css/style.css'); ?>" />
+	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script type="text/javascript" src="<?php echo Util::base_url('js/vote.js'); ?>"></script>
 
 </head>
 <body>
